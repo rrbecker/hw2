2be529277f8c7691b47d3f3684e4afbdcecbb409
rrbecker <35316419+rrbecker@users.noreply.github.com>
2018-02-14 15:53:46 -0600

    Updated index.js
    Updated index.js

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 // Your code goes here:
 let handleImageClick = function(event) {
-  // let theImage = jQuery(this)
   let theImage = jQuery(event.target)
   console.log("Got a click event:", event)
   let srcValue = theImage.attr("src")
-  console.log(srcValue)
-}
-
+  console.log(srcValue)}
 
 jQuery("img").on("click", handleImageClick)
